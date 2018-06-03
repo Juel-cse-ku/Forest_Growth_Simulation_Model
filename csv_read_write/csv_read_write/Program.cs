11c7ebff7a1e0210c1e66252812a7f7690602052
Md. Juel Hossain <25183291+Juel-cse-ku@users.noreply.github.com>
2018-06-03 07:29:28 +0600

    radius of FON/ZOI added

--- csv_read_write/csv_read_write/Program.cs
+++ csv_read_write/csv_read_write/Program.cs
@@ -14,6 +14,14 @@ namespace csv_read_write
         {
             public double X { get; set; }
             public double Y { get; set; }
+            public double R { get; set; }
+            public double H { get; set; }
+            public double a { get; set; }
+            public double b { get; set; }
+            public double dbh { get; set; }
+            public double AGE { get; set; }
+            public double Species { get; set; }
+            public double ZOI { get; set; }
 
             public static Tree FromLine(string line)
             {
@@ -23,8 +31,26 @@ namespace csv_read_write
                 {
                     X = double.Parse(data[0]),
                     Y = double.Parse(data[1]),
+                    Species = double.Parse(data[2]),
+                    dbh = double.Parse(data[3]),
+                    AGE = double.Parse(data[4]),
+                    a = double.Parse(data[5]),
+                    b = double.Parse(data[6]),
+                    H = double.Parse(data[7]),
+                    R = Get_R(double.Parse(data[5]), double.Parse(data[6]), double.Parse(data[3])),
+                    //ZOI = Get_ZOI(R),
                 };
             }
+
+            static double Get_R(double a,double b,double dbh)
+            {
+                return (a * (Math.Pow(dbh / 2, b)));
+            }
+            static double Get_ZOI(object obj)
+            {
+                var R = Convert.ToDouble(obj);
+                return (3.1416 * Math.Pow(R, 2));
+            } 
         }
         static void Main(string[] args)
         {
@@ -32,14 +58,14 @@ namespace csv_read_write
             var trees = ReadTrees(args[0]);
 
             var text = new StringBuilder();
-            var header = string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", "X", "Y","species","dbh","a","b","AGE","H");
+            var header = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", "X", "Y","species","dbh","AGE", "a", "b", "H","R");
             text.AppendLine(header);
 
             foreach (var tree in trees)
             {
                 //Console.WriteLine(tree.X);
                // Console.WriteLine(tree.Y);
-                var newLine = string.Format("{0},{1}", tree.X, tree.Y);
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", tree.X, tree.Y,tree.Species,tree.dbh,tree.AGE,tree.a,tree.b,tree.H,tree.R);
                 text.AppendLine(newLine);
             }
             File.WriteAllText("../../../DATA/output.csv", text.ToString());
