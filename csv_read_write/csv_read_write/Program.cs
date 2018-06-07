5455d49c6713df2770ae75e9ca703f5f31fc36ab
Md. Juel Hossain <25183291+Juel-cse-ku@users.noreply.github.com>
2018-06-08 03:56:19 +0600

    FA saved to CSV file

--- csv_read_write/csv_read_write/Program.cs
+++ csv_read_write/csv_read_write/Program.cs
@@ -59,19 +59,19 @@ namespace csv_read_write
             args = new[] { "../../../DATA/input.csv" };
             var trees = ReadTrees(args[0]);
             var zoi_trees = ZOI_Trees(trees);
+            var FA_trees = CalculateFA(trees);
 
-            CalculateFA(trees);
-
+            Console.WriteLine(FA_trees.Count());
 
             var text = new StringBuilder();
-            var header = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}", "X", "Y","species","dbh","rbh","AGE", "a", "b", "H","R","ZOI");
+            var header = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}", "X", "Y","species","dbh","rbh","AGE", "a", "b", "H","R","ZOI","FA");
             text.AppendLine(header);
 
             for (int i=0;i<trees.Count();i++)
             {
                 //Console.WriteLine(tree.X);
                // Console.WriteLine(tree.Y);
-                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}", trees[i].X, trees[i].Y, trees[i].Species, trees[i].dbh,trees[i].rbh, trees[i].AGE, trees[i].a, trees[i].b, trees[i].H, trees[i].R, zoi_trees[i].ZOI);
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}", trees[i].X, trees[i].Y, trees[i].Species, trees[i].dbh,trees[i].rbh, trees[i].AGE, trees[i].a, trees[i].b, trees[i].H, trees[i].R, zoi_trees[i].ZOI,FA_trees[i].FA);
                 text.AppendLine(newLine);
             }
             foreach(var d_tree in zoi_trees)
@@ -113,21 +113,22 @@ namespace csv_read_write
         {
             var list = new List<Tree>();
 
-            for (int k = 0; k < trees.Count(); ++k)
+            for (int k = 0; k < trees.Count(); k++)
             {
                 //Console.WriteLine(trees[k].FA);
-                for (int n = k + 1; n < trees.Count(); ++n)
+                for (int n = k + 1; n < trees.Count(); n++)
                 {
                     trees[k].FA = trees[k].FA + FA_kn(trees[k], trees[n]);
                     trees[n].FA = trees[n].FA + FA_kn(trees[n], trees[k]);
                 }
                 
                 trees[k].FA = trees[k].FA / (3.1416 * Math.Pow(trees[k].R, 2));
-                Console.WriteLine(trees[k].FA);
+                //Console.WriteLine(trees[k].FA);
                 Tree tree = new Tree()
                 {
                     FA = trees[k].FA,
                 };
+                list.Add(tree);
             }
             return list;    //FA of all trees
         }
@@ -159,7 +160,7 @@ namespace csv_read_write
             double dr = (r1 - r0) / nStrides;
             double r = r0 + (dr / 2);
 
-            for (int i = 1; i <= nStrides; ++i)
+            for (int i = 0; i < nStrides; i++)
             {
                 double x = (Math.Pow(A, 2) + Math.Pow(r, 2) - Math.Pow(TreeK.R, 2)) / (2 * A);
                 double y = Math.Sqrt(Math.Pow(r, 2) - Math.Pow(x, 2));
